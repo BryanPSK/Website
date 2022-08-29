acb493dc12d9ccf79f86e5a64ea7a9e51c167681
BryanPSK <48677467+BryanPSK@users.noreply.github.com>
2022-08-30 01:58:08 +0800

    Updates

---
+++ precache-manifest.a886e63c0c912c8058640002046507c2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "09727e7d725f09f2f903",
+    "url": "/bryanpsk.github.io/static/css/main.21e7ab36.chunk.css"
+  },
+  {
+    "revision": "09727e7d725f09f2f903",
+    "url": "/bryanpsk.github.io/static/js/main.09727e7d.chunk.js"
+  },
+  {
+    "revision": "b74c6bea94ebc2dd106c",
+    "url": "/bryanpsk.github.io/static/js/1.b74c6bea.chunk.js"
+  },
+  {
+    "revision": "dbdb9ca9ffcad91dbafc",
+    "url": "/bryanpsk.github.io/static/js/2.dbdb9ca9.chunk.js"
+  },
+  {
+    "revision": "42e03778ea3263854553",
+    "url": "/bryanpsk.github.io/static/js/runtime~main.42e03778.js"
+  },
+  {
+    "revision": "3238faad9fc80049e61b3b9b1edabfce",
+    "url": "/bryanpsk.github.io/static/media/video.3238faad.mp4"
+  },
+  {
+    "revision": "4fe42b6a2f83e05926b285e532b2a59b",
+    "url": "/bryanpsk.github.io/static/media/bg4.4fe42b6a.jpg"
+  },
+  {
+    "revision": "f2fac8874aa876851f7c1e42760c67d9",
+    "url": "/bryanpsk.github.io/static/media/bg5.f2fac887.jpg"
+  },
+  {
+    "revision": "a383143bd1456e028969e50f28986641",
+    "url": "/bryanpsk.github.io/static/media/display.a383143b.jpg"
+  },
+  {
+    "revision": "0ba78cdbf953d044dcd8c4de4a4524c9",
+    "url": "/bryanpsk.github.io/static/media/bg1.0ba78cdb.jpg"
+  },
+  {
+    "revision": "405c07f78de0459e0910196ab5588ca7",
+    "url": "/bryanpsk.github.io/static/media/bg2.405c07f7.jpg"
+  },
+  {
+    "revision": "412623347f50ff92af45a89227678520",
+    "url": "/bryanpsk.github.io/static/media/bg3.41262334.jpg"
+  },
+  {
+    "revision": "a5824f4feba716ef491098154da563ed",
+    "url": "/bryanpsk.github.io/static/media/bg6.a5824f4f.jpg"
+  },
+  {
+    "revision": "1ea9245ef1941bb63ba54c4835a64a96",
+    "url": "/bryanpsk.github.io/static/media/projects.1ea9245e.jpg"
+  },
+  {
+    "revision": "ce9c993d276b4da3d275061f486872b3",
+    "url": "/bryanpsk.github.io/static/media/ETLAS.ce9c993d.jpg"
+  },
+  {
+    "revision": "b182a4883375fa9e550533c227fd4794",
+    "url": "/bryanpsk.github.io/static/media/laundryApp.b182a488.jpg"
+  },
+  {
+    "revision": "864b09ed089bcd3b7e5f62e548f5eecd",
+    "url": "/bryanpsk.github.io/static/media/PESULA-2.864b09ed.mp4"
+  },
+  {
+    "revision": "e6d5fed8cd5df61bb04c87dd78bc6d0e",
+    "url": "/bryanpsk.github.io/static/media/pesula.e6d5fed8.mp4"
+  },
+  {
+    "revision": "3f3d5c51d8b4a0c247580e985d8c98ac",
+    "url": "/bryanpsk.github.io/index.html"
+  }
+];
